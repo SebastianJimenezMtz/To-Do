045c9c73bec232e56d09781417954f0f17696087
Roman Ortega <roman88129@gmail.com>
2025-05-20 12:53:33 -0600

    Added description sequelize

--- backend/src/models/taskList.model.js
+++ backend/src/models/taskList.model.js
@@ -22,6 +22,10 @@ const TaskList = sequelize.define(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    Description: { // Adding the Description field
+      type: DataTypes.TEXT, // VARCHAR(MAX) translates to TEXT in Sequelize
+      allowNull: true, // Assuming Description can be optional
+    },
     CreatedAt: {
       type: DataTypes.DATE,
       defaultValue: DataTypes.NOW,
