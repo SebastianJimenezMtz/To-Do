fff477b11380030c861753522fd99fc6b74968fd
SebastianJimenezMtz <158785093+SebastianJimenezMtz@users.noreply.github.com>
2025-05-21 15:34:34 -0600

    Update apiConfig.js

--- frontend/to-do-front/src/apiConfig.js
+++ frontend/to-do-front/src/apiConfig.js
@@ -1,2 +1,2 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001/api'; // Fallback para desarrollo local
+const API_URL = process.env.REACT_APP_API_URL; // Fallback para desarrollo local
 export default API_URL;
